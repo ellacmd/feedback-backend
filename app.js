7a9de29d24c52a1e9fcf1c9b82cc6fcc415d888a
exploitenomah <iksploiting@gmail.com>
2023-10-19 16:54:37 +0100

    add route for comments

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan")
 require("dotenv").config()
 var indexRouter = require("./routes/index")
 var usersRouter = require("./routes/users")
+var commentsRouter = require("./routes/comments")
 var productRequestsRouter = require("./routes/productRequests")
 
 var app = express()
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
 app.use("/product-requests", productRequestsRouter)
+app.use("/comments", commentsRouter)
 
 app.use(function (req, res, next) {
   next(createError(404))
