9c1e65329631b9575a81cb55a135fdc060584825
exploitenomah <iksploiting@gmail.com>
2023-10-19 22:36:42 +0100

    refactor

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,17 @@
 const { routeTryCatcher } = require("../utils/controller.js")
-const { verifyJwt } = require("../utils/security.js")
+const { validateToken } = require("../utils/security.js")
 const User = require("../models/user")
 
 module.exports.authorize = routeTryCatcher(async function (req, res, next) {
   const token =
     req.headers["authorization"]?.split(" ")?.[1] || req.headers.access_token
   if (!token) return next("Not Allowed")
-  const payload = verifyJwt(token)
-  if (new Date(Date.now()) - new Date(payload.iat * 1000) > 8.64e7)
-    return next("Not Allowed")
-  const user = await User.findById(payload._id)
+  const user = validateToken(token)
   if (!user) return next("Not Allowed!")
   req.user = user
   next()
 })
+
 module.exports.authorizeAdmin = routeTryCatcher(async function (
   req,
   res,
