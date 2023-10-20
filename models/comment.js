874d398344b98c61fb85a638c6665e344a548aa5
exploitenomah <iksploiting@gmail.com>
2023-10-20 05:55:36 +0100

    remove user password from comment user path

--- models/comment.js
+++ models/comment.js
@@ -1,29 +1,35 @@
 const mongoose = require("mongoose")
 
-const commentSchema = new mongoose.Schema({
-  content: { type: String, maxLength: 250 },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User Id is required!"],
+const commentSchema = new mongoose.Schema(
+  {
+    content: { type: String, maxLength: 250 },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User Id is required!"],
+    },
+    productRequest: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductRequest",
+      required: [true, "Product Request Id is required!"],
+    },
   },
-  productRequest: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ProductRequest",
-    required: [true, "Product Request Id is required!"],
-  },
-}, {
-  collation: { locale: 'en', strength: 2 },
-  toJSON: {
-    virtuals: true
+  {
+    collation: { locale: "en", strength: 2 },
+    toJSON: {
+      virtuals: true,
+    },
   }
-})
+)
 
 commentSchema.pre(/^find/, async function (next) {
   if (this.options._recursed) {
     return next()
   }
-  this.populate({ path: "user", options: { _recursed: true } })
+  this.populate({
+    path: "user",
+    options: { _recursed: true, select: "-password" },
+  })
   next()
 })
 
