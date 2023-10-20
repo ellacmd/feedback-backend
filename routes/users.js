e0b38ab41a156a1c0de0e8e62f62ed3b83e65c29
exploitenomah <iksploiting@gmail.com>
2023-10-20 10:10:14 +0100

    remove comment

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ const { authorize } = require("../middleware/auth")
 router.post("/", signupUser, sendResponse)
 router.get("/", loginUser, sendResponse)
 router.get("/all", searchForUsers, sendResponse)
-//authorize
+
 router.use(authorize)
 router.get("/:id", getUser, sendResponse)
 router.put("/:id", checkIfIsOwnerAndUserOfIdParam, updateUser, sendResponse)
