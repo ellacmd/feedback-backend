67cc733fa6ca87dad4e39d8faa6046865503e549
exploitenomah <iksploiting@gmail.com>
2023-10-20 10:13:13 +0100

    create reply model

---
+++ models/reply.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+const replySchema = new mongoose.Schema(
+  {
+    content: { type: String, maxLength: 250 },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User Id is required!"],
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      required: [true, "Comment Id is required!"],
+    },
+  },
+  {
+    collation: { locale: "en", strength: 2 },
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+)
+
+replySchema.pre(/^find/, async function (next) {
+  if (this.options._recursed) {
+    return next()
+  }
+  this.populate({
+    path: "user",
+    options: { _recursed: true, select: "-password" },
+  })
+  next()
+})
+
+module.exports = mongoose.model("Reply", replySchema)
